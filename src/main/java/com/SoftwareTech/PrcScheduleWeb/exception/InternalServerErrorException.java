34f40c4d7cae25664bd1d583a3785025d392e5b8
levandung-it03 <levandung.it03@gmail.com>
2024-02-06 20:21:16 +0700

    Update API Exception Objects by levandung

---
+++ src/main/java/com/SoftwareTech/PrcScheduleWeb/exception/InternalServerErrorException.java
@@ -0,0 +1,11 @@
+package com.SoftwareTech.PrcScheduleWeb.exception;
+
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
