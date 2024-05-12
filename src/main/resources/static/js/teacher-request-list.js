860dd063aa2970f619b044453902954b31fa32ca
levandung-it03 <levandung.it03@gmail.com>
2024-05-13 00:47:42 +0700

    Complete Project by levandung

--- src/main/resources/static/js/teacher-request-list.js
+++ src/main/resources/static/js/teacher-request-list.js
@@ -8,36 +8,43 @@ const plainTableRows = [...$$('table tbody tr')];
     customizeSortingListEvent();
     buildHeader();
     customizeDenyingTeacherRequestAction();
+    customizeCancelingTeacherRequestAction();
 })();
 
 function customizeDenyingTeacherRequestAction() {
-    const denyBtn = $('button[name=denyTeacherRequestBtn]');
-    if (denyBtn != null)
-        denyBtn.addEventListener("click", e => {
-            const denyingForm = $('form#deny-request');
-            //--Set value for 'input name="requestId"'.
-            denyingForm.querySelector('input[name=requestId]').value = denyBtn.id;
-
-            //--Get value for 'input name="interactionReason"' from manager.
-            denyingForm.querySelector('input[name=interactionReason]').value = prompt("Vui lòng nhập lý do");
-            if (confirm("Bạn chắc chắn muốn thực hiện thao tác?") == true) {
-                denyingForm.submit();
-            }
+    const denyBtns = $$('button[name=denyTeacherRequestBtn]');
+    if (denyBtns != null){
+        denyBtns.forEach(btn => {
+            btn.addEventListener("click", e => {
+                const denyingForm = $('form#deny-request');
+                //--Set value for 'input name="requestId"'.
+                denyingForm.querySelector('input[name=requestId]').value = btn.id;
+    
+                //--Get value for 'input name="interactionReason"' from manager.
+                denyingForm.querySelector('input[name=interactionReason]').value = prompt("Vui lòng nhập lý do");
+                if (confirm("Bạn chắc chắn muốn thực hiện thao tác?") == true) {
+                    denyingForm.submit();
+                }
+            })
         });
+    }
 }
 
-function customizeDenyingTeacherRequestAction() {
-    const cancelBtn = $('button[name=cancelTeacherRequestBtn]');
-    if (cancelBtn != null)
-        cancelBtn.addEventListener("click", e => {
-            const cancelingForm = $('form#cancel-request');
-            //--Set value for 'input name="requestId"'.
-            cancelingForm.querySelector('input[name=requestId]').value = cancelBtn.id;
-
-            //--Get value for 'input name="interactionReason"' from manager.
-            cancelingForm.querySelector('input[name=interactionReason]').value = prompt("Vui lòng nhập lý do");
-            if (confirm("Bạn chắc chắn muốn thực hiện thao tác?") == true) {
-                cancelingForm.submit();
-            }
+function customizeCancelingTeacherRequestAction() {
+    const cancelBtns = $$('button[name=cancelTeacherRequestBtn]');
+    if (cancelBtns != null){
+        cancelBtns.forEach(btn => {
+            btn.addEventListener("click", e => {
+                const cancelingForm = $('form#cancel-request');
+                //--Set value for 'input name="requestId"'.
+                cancelingForm.querySelector('input[name=requestId]').value = btn.id;
+    
+                //--Get value for 'input name="interactionReason"' from manager.
+                cancelingForm.querySelector('input[name=interactionReason]').value = prompt("Vui lòng nhập lý do");
+                if (confirm("Bạn chắc chắn muốn thực hiện thao tác?") == true) {
+                    cancelingForm.submit();
+                }
+            })
         });
+    }
 }
